42e0f98a03f89b28a904bb3c468e304d1cf94dae
pianonerd7 <pianonerd7@gmail.com>
2015-06-01 22:25:52 -0400

    Can get real time data from accelerometer

--- app/src/main/java/pianonerd7/distance/MainActivity.java
+++ app/src/main/java/pianonerd7/distance/MainActivity.java
@@ -1,18 +1,59 @@
 package pianonerd7.distance;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.hardware.SensorManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.widget.TextView;
+import android.hardware.SensorEventListener;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mySensorManager;
+    private Sensor mySensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mySensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mySensor = mySensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mySensorManager.registerListener(this, mySensor, mySensorManager.SENSOR_DELAY_NORMAL);
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        //Sensor sensor = sensorEvent.sensor;
+
+        //if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+        TextView textx = (TextView) findViewById(R.id.xText);
+        TextView texty = (TextView) findViewById(R.id.yText);
+        TextView textz = (TextView) findViewById(R.id.zText);
+
+        textx.setText(x+"");
+        texty.setText(y+"");
+        textz.setText(z+"");
+
+        //}
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
